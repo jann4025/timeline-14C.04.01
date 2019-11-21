17a932de8f606a734bdb5e7a4b560deffb2f7b1e
Sarah Davidsen <sarahdavidsen@laptop-i7i6i9vd.kea.local>
2019-11-21 09:40:27 +0100

    try to merge with master

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-import "babel-core/register";
-import "babel-polyfill";
+// import "babel-core/register";
+// import "babel-polyfill";
 
-("use strict");
+// ("use strict");
 
-let timeLineInfo;
+// let timeLineInfo;
 
-getData();
+// getData();
 
-function getData() {
-  let fetchData = fetch("https://v2-api.sheety.co/2d953e1a09a4d62db89c0f444ce7a8dd/timeline/timeline?fbclid=IwAR1BdZx057x8y2AYjnoIaza5YN9cP7il5o61rNQigy8qcOdyhUhYwIo4mus");
-  fetchData
-    .then(response => {
-      //   console.log(response);
-      return response.json();
-    })
-    .then(data => {
-      timeLineInfo = data.timeline;
-      console.log(timeLineInfo);
-    });
+// function getData() {
+//   let fetchData = fetch("https://v2-api.sheety.co/2d953e1a09a4d62db89c0f444ce7a8dd/timeline/timeline?fbclid=IwAR1BdZx057x8y2AYjnoIaza5YN9cP7il5o61rNQigy8qcOdyhUhYwIo4mus");
+//   fetchData
+//     .then(response => {
+//       //   console.log(response);
+//       return response.json();
+//     })
+//     .then(data => {
+//       timeLineInfo = data.timeline;
+//       console.log(timeLineInfo);
+//     });
 
-  displayData();
-}
+//   displayData();
+// }
 
-function displayData() {
-  timeLineInfo.forEach(day => {
-    // console.log(day);
-  });
+// function displayData() {
+//   timeLineInfo.forEach(day => {
+//     // console.log(day);
+//   });
 
-  document.querySelector(`#day${day.id}.textContent = ${day.day};`);
-}
+//   document.querySelector(`#day${day.id}.textContent = ${day.day};`);
+// }
